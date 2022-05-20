b14936e4b950b9cf3e922878664952f120782657
Matt Keane <mattpkeane@gmail.com>
2022-05-20 12:38:58 -0500

    creating a game now displays access code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import './App.css';
-import { useRef, useEffect } from 'react'
+import { 
+  useRef, 
+  useEffect,
+  useState 
+} from 'react'
 import { io } from 'socket.io-client'
 import Start from './Start'
 
 function App() {
   const socket = useRef(null)
+  const [roomCode, setRoomCode] = useState(null)
   const { REACT_APP_API_URL } = process.env
 
   useEffect(() => {
     socket.current = io(REACT_APP_API_URL)
-    socket.current.on('accessCode', accessCode => console.log(accessCode))
+    socket.current.on('accessCode', accessCode => setRoomCode(accessCode))
   }, [REACT_APP_API_URL])
 
 
 
   return (
-    <div className="App">
-      <Start socket={ socket } />
-    </div>
+    <>
+    {
+      roomCode
+      ?
+      <p>{ roomCode }</p>
+      :
+      <div className="App">
+        <Start socket={ socket } />
+      </div>
+    }
+    </>
   );
 }
 
