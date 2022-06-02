5035bfe4c00c3809f1734620c6379ea444aba654
Matt Keane <mattpkeane@gmail.com>
2022-06-02 11:04:10 -0500

    connects to namespaced back end server

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const { REACT_APP_API_URL } = process.env
 
   useEffect(() => {
-    socket.current = io(REACT_APP_API_URL)
+    socket.current = io(REACT_APP_API_URL + '/main')
     socket.current.on('accessCode', accessCode => setRoomCode(accessCode))
     socket.current.on('updateRoom', updatedRoom => setRoom(updatedRoom))
   }, [REACT_APP_API_URL])
